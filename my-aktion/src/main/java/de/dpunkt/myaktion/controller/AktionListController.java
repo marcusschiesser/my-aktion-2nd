47d4eab98b0bb908f87d964bb9fa35d5341aefa8
marcus <mail@marcusschiesser.de>
2012-03-26 22:21:57 +0200

    Button hinzugefügt um eine Aktion zu löschen

--- my-aktion/src/main/java/de/dpunkt/myaktion/controller/AktionListController.java
+++ my-aktion/src/main/java/de/dpunkt/myaktion/controller/AktionListController.java
@@ -12,27 +12,31 @@ import de.dpunkt.myaktion.model.Aktion;
 public class AktionListController {
 	@Inject
 	private AktionEditController aktionEditController;
-	
+
 	@Inject
 	private SpendeListController spendeListController;
-	
+
 	public String doAddAktion() {
 		aktionEditController.setAktionToEdit(Mode.ADD);
 		return Pages.AKTION_EDIT;
 	}
-	
+
 	public String doEditAktion(Aktion aktion) {
 		aktionEditController.setAktionToEdit(Mode.EDIT, aktion);
 		return Pages.AKTION_EDIT;
 	}
-	
+
 	public String doEditSpendeForm(Aktion aktion) {
 		return Pages.SPENDE_FORM_EDIT;
 	}
-	
+
 	public String doListSpende(Aktion aktion) {
 		spendeListController.setAktion(aktion);
 		return Pages.SPENDE_LIST;
+	}
+
+	public void doDeleteAktion(Aktion aktion) {
+		System.out.println("Aktion löschen noch nicht implementiert");
 	}
 
 }
