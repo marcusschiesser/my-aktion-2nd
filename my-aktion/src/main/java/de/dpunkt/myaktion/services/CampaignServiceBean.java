af5650146d67d147ccba6bb9d6967e078f652f18
marcus.schiesser <mail@marcusschiesser.de>
2014-03-28 18:14:49 +0800

    6.4.2	Services um Transaktionen erweitern

--- my-aktion/src/main/java/de/dpunkt/myaktion/services/CampaignServiceBean.java
+++ my-aktion/src/main/java/de/dpunkt/myaktion/services/CampaignServiceBean.java
@@ -2,13 +2,13 @@ package de.dpunkt.myaktion.services;
 
 import de.dpunkt.myaktion.model.Campaign;
 
-import javax.enterprise.context.RequestScoped;
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
-@RequestScoped
+@Stateless
 public class CampaignServiceBean implements CampaignService {
     @Inject
     EntityManager entityManager;
