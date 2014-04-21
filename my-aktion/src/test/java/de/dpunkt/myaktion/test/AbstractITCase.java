25d5de28f79616b1702ba0a939a4e28267ecb0e5
marcus.schiesser <mail@marcusschiesser.de>
2014-04-21 17:38:20 +0800

    Fuer die Tests eine eigene In-Memory-Datasource benutzen

--- my-aktion/src/test/java/de/dpunkt/myaktion/test/AbstractITCase.java
+++ my-aktion/src/test/java/de/dpunkt/myaktion/test/AbstractITCase.java
@@ -8,9 +8,16 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import java.io.File;
 
 public abstract class AbstractITCase {
+
     @Deployment(testable = false)
     public static WebArchive createDeployment() {
-        return ShrinkWrap.create(ZipImporter.class, "test.war").importFrom(new File("target/my-aktion.war"))
+        WebArchive archive = ShrinkWrap.create(ZipImporter.class, "test.war").importFrom(new File("target/my-aktion.war"))
                 .as(WebArchive.class);
+        archive.delete("/WEB-INF/classes/META-INF/persistence.xml");
+        archive.delete("/WEB-INF/myaktion-ds.xml");
+        archive.addAsResource("META-INF/test-persistence.xml",
+                "META-INF/persistence.xml");
+        archive.addAsWebInfResource("test-ds.xml", "test-ds.xml");
+        return archive;
     }
 }
