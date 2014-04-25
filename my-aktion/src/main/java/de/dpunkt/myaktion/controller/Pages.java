5427b93a93634c2802b90e7f322070821c6d1cb1
marcus.schiesser <mail@marcusschiesser.de>
2014-04-25 20:20:44 +0800

    7.2.3	Benutzeroberfläche absichern

--- my-aktion/src/main/java/de/dpunkt/myaktion/controller/Pages.java
+++ my-aktion/src/main/java/de/dpunkt/myaktion/controller/Pages.java
@@ -2,10 +2,10 @@ package de.dpunkt.myaktion.controller;
 
 public class Pages {
 
-    public static final String LIST_CAMPAIGNS = "listCampaigns";
-    public static final String EDIT_CAMPAIGN = "editCampaign";
-    public static final String EDIT_DONATION_FORM = "editDonationForm";
-    public static final String LIST_DONATIONS = "listDonations";
+    public static final String LIST_CAMPAIGNS = "/organizer/listCampaigns";
+    public static final String EDIT_CAMPAIGN = "/organizer/editCampaign";
+    public static final String EDIT_DONATION_FORM = "/organizer/editDonationForm";
+    public static final String LIST_DONATIONS = "/organizer/listDonations";
     public static final String DONATE_MONEY = "donateMoney";
 
 }
